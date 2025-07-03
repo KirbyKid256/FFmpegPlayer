ccebbc9304d029057b59c3a7d4b198d15bc6e161
KirbyKidJ <70983335+KirbyKid256@users.noreply.github.com>
2025-07-03 14:36:45 +0000

    Okay this isn't reliable
    Linux VM is still slow for me
    - Downgraded Linux and MacOS FFmpeg Libraries to match Windows

--- test/test.gd
+++ test/test.gd
@@ -1,10 +1,15 @@
 extends Node2D
 
+@onready var file_dialog: FileDialog = $FileDialog
+
 @onready var theora_ogv: VideoStreamPlayer = $TheoraOGV
 @onready var ffmpeg_ogv: VideoStreamPlayer = $FFmpegOGV
 @onready var ffmpeg_mp4: VideoStreamPlayer = $FFmpegMP4
 
 func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_open"):
+		file_dialog.popup()
+	
 	if event.is_action_pressed("ui_accept"):
 		if theora_ogv.is_playing():
 			theora_ogv.stop()
@@ -14,3 +19,8 @@ func _input(event: InputEvent) -> void:
 			theora_ogv.play()
 			ffmpeg_ogv.play()
 			ffmpeg_mp4.play()
+
+func _on_file_dialog_file_selected(path: String) -> void:
+	theora_ogv.stream = load(path.get_basename() + ".ogv")
+	ffmpeg_ogv.stream = load(path.get_basename() + ".ogv") as VideoStreamFFmpeg
+	ffmpeg_mp4.stream = load(path)
